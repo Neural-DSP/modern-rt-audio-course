10289f153f69d92380c452d6388d7ac61483c623
Joao Rossi <joao@neuraldsp.com>
2023-08-27 19:36:20 +0300

    add summing methods to ramp

--- dsp/Ramp.h
+++ dsp/Ramp.h
@@ -44,11 +44,69 @@ public:
         if (std::abs(newTargetValue - currentValue) > minDelta)
         {
             targetValue = newTargetValue;
-            rampStep = (targetValue - currentValue) / (sampleRate * rampTime);
+            rampStep = (targetValue - currentValue) / static_cast<F>(sampleRate * rampTime);
         }
 
         if (skipRamp)
             currentValue = targetValue = newTargetValue;
+    }
+
+    // Apply summing ramp to a single sample in-place
+    void applySum(F* buffers, unsigned int numChannels)
+    {
+        const F targetDelta { std::fabs(targetValue - currentValue) };
+        if ((targetDelta > std::fabs(static_cast<F>(2) * rampStep)) && (std::fabs(rampStep) > minDelta))
+            currentValue += rampStep;
+        else
+            currentValue = targetValue;
+
+        for (unsigned int ch = 0; ch < numChannels; ++ch)
+            buffers[ch] += currentValue;
+    }
+
+    // Apply summing ramp to an audio buffer in-place
+    void applySum(F* const* buffers, unsigned int numChannels, unsigned int numSamples)
+    {
+        for (unsigned int n = 0; n < numSamples; ++n)
+        {
+            const F targetDelta { std::fabs(targetValue - currentValue) };
+            if ((targetDelta > std::fabs(static_cast<F>(2) * rampStep)) && (std::fabs(rampStep) > minDelta))
+                currentValue += rampStep;
+            else
+                currentValue = targetValue;
+
+            for (unsigned int ch = 0; ch < numChannels; ++ch)
+                buffers[ch][n] += currentValue;
+        }
+    }
+
+    // Apply summing ramp to an audio buffer out-of-place
+    void applySum(F* const* output, const F* const* input, unsigned int numChannels, unsigned int numSamples)
+    {
+        for (unsigned int n = 0; n < numSamples; ++n)
+        {
+            const F targetDelta { std::fabs(targetValue - currentValue) };
+            if ((targetDelta > std::fabs(static_cast<F>(2) * rampStep)) && (std::fabs(rampStep) > minDelta))
+                currentValue += rampStep;
+            else
+                currentValue = targetValue;
+
+            for (unsigned int ch = 0; ch < numChannels; ++ch)
+                output[ch][n] = currentValue + input[ch][n];
+        }
+    }
+
+    // Apply gain ramp to an audio buffer in-place for single sample
+    void applyGain(F* buffers, unsigned int numChannels)
+    {
+        const F targetDelta { std::fabs(targetValue - currentValue) };
+        if ((targetDelta > std::fabs(static_cast<F>(2) * rampStep)) && (std::fabs(rampStep) > minDelta))
+            currentValue += rampStep;
+        else
+            currentValue = targetValue;
+
+        for (unsigned int ch = 0; ch < numChannels; ++ch)
+            buffers[ch] *= currentValue;
     }
 
     // Apply gain ramp to an audio buffer in-place
@@ -89,7 +147,7 @@ public:
     // Minimun absolute differente between target and current value
     static constexpr F minDelta { static_cast<F>(1e-9) };
 
-public:
+private:
     double sampleRate { 48000.0 };
     F rampTime;
     F rampStep { static_cast<F>(0) };
